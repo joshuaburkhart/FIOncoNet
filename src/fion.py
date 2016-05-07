6edef33946fb650fe50d8e2610ee904b34fa1ca7
Joshua Burkhart <burkhart.joshua@gmail.com>
2016-05-06 18:59:28 -0700

    updating architecture, working to match

---
+++ src/fion.py
@@ -0,0 +1,15 @@
+
+"""
+---------
+S = Study(op(options))
+For vf in vfr(S)
+    If vfv.valid(vf)
+        G = Group(vf)
+        For dl in vfp(vfr).data_lines
+            M = Sample(dl.arg1 to dl.argN)
+            G.add(M)
+        S.add(G)
+    Else
+        Error
+
+"""
